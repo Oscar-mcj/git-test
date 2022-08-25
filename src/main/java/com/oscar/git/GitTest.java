5901155f41f87a8a36d8af0913e9c7b2b07a11aa
Oscar <1621990228@qq.com>
2022-08-25 17:58:48 +0800

    first commit

---
+++ src/main/java/com/oscar/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.oscar.git;
+
+/**
+ * @author Oscar
+ * @create 2022-08-25 17:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
