e694ce958d937fdfe91b0025f17fc152017e52fd
Oscar <1621990228@qq.com>
2022-08-25 23:12:58 +0800

    main commit1

--- src/main/java/com/oscar/git/GitTest.java
+++ src/main/java/com/oscar/git/GitTest.java
@@ -6,7 +6,7 @@ package com.oscar.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git main!");
         System.out.println("hello git2");
         System.out.println("hello git3");
     }
